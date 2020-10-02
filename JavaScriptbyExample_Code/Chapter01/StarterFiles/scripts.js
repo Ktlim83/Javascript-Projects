b10233d3dfbba63edf8b48a0e5248ed3f1e42d51
Keith Lim <kweef206@gmail.com>
2020-10-01 20:10:30 -0700

    add files

---
+++ JavaScriptbyExample_Code/Chapter01/StarterFiles/scripts.js
@@ -0,0 +1,48 @@
+class ToDoClass {
+    constructor() {
+        this.tasks = [{
+                task: 'Go to Dentist',
+                isComplete: false
+            },
+            {
+                task: 'Do Gardening',
+                isComplete: true
+            },
+            {
+                task: 'Renew Library Account',
+                isComplete: false
+            },
+        ];
+        this.loadTasks();
+    }
+
+    loadTasks() {
+        let tasksHtml = this.tasks.reduce((html, task, index) => html +=
+            this.generateTaskHtml(task, index), '');
+        document.getElementById('taskList').innerHTML = tasksHtml;
+    }
+
+    generateTaskHtml(task, index) {
+        return `
+         <li class="list-group-item checkbox">
+          <div class="row">
+           <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+            <label><input id="toggleTaskStatus" type="checkbox"  
+            onchange="toDo.toggleTaskStatus(${index})" value="" class="" 
+            ${task.isComplete?'checked':''}></label>
+           </div>
+           <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
+            ${task.task}
+          </div>
+          <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+            <a class="" href="/" onClick="toDo.deleteTask(event, ${index})"><i 
+            id="deleteTask" data-id="${index}" class="delete-icon glyphicon 
+            glyphicon-trash"></i></a>
+           </div>
+          </div>
+         </li>
+       `;
+    }
+
+
+}
---
+++ ToDo/script.js
@@ -0,0 +1,8 @@
+class ToDoClass {
+    constructor() {
+        alert('Hello World!');
+    }
+}
+window.addEventListener("click", function () {
+    var toDo = new ToDoClass();
+});
